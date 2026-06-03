22e56b640bd294fcd8b9b4b26a7a03ee63730272
kerickson-dot <kerickson@digitalocean.com>
2026-06-03 09:33:36 -0600

    Fix greeting message formatting in main.go

--- main.go
+++ main.go
@@ -37,7 +37,7 @@ func logRequest(r *http.Request) {
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		logRequest(r)
-		fmt.Fprintf(w, "Well hello there! you've requested %s\n", r.URL.Path)
+		fmt.Fprintf(w, "Well, hello there! you've requested %s\n", r.URL.Path)
 	})
 
 	http.HandleFunc("/cached", func(w http.ResponseWriter, r *http.Request) {
